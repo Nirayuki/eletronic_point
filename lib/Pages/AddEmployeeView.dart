f8540e56bb5325477e6690067f4462b2833202ea
Aron Felippe Kerkhoven <aronkerk8@gmail.com>
2022-05-10 10:13:14 -0300

    Adding textfield

--- lib/Pages/AddEmployeeView.dart
+++ lib/Pages/AddEmployeeView.dart
@@ -14,56 +14,53 @@ class _AddEmployeeViewState extends State<AddEmployeeView> {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
-            title: Row(children: [
-              InkWell(
-                onTap: (){
-                  Navigator.of(context).pushNamed('/');
-                },
-                child: Icon(Icons.assignment_return),
-              ),
-              Container(width: 30,),
-              Text("Adicionar Funcionário"),
-            ],),
-            actions: [
-
-
-            ],
+            title: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).pushNamed('/');
+                  },
+                  child: Icon(Icons.assignment_return),
+                ),
+                Container(
+                  width: 30,
+                ),
+                Text("Adicionar Funcionário"),
+              ],
+            ),
+            actions: [],
           ),
-          body: ListView.builder(
-              itemBuilder: (BuildContext build,int index) {
-
-                return Card(
-                    color: Colors.green,
-
-                    child: InkWell(
-                      splashColor: Colors.red.withAlpha(50),
-                      onTap: (){
-
-                      },
-                      child: SizedBox(
-                        height: 160,
-
-                        child: Center(
-                          child: Text(
-                            "Elenilton Deddddddddzengrini",
-                            style: TextStyle(fontSize: 40),
-                          ),
-                        ),
-                      ),
-                    )
-                );
-
-
-              }
+          body: Padding(
+            padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Digite o nome aqui",
+                    labelText: "Nome do colaborador",
+                    labelStyle: TextStyle(fontSize: 17),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                Container(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Teste",
+                    labelText: "teste",
+                    labelStyle: TextStyle(fontSize: 17),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
           ),
           floatingActionButton: FloatingActionButton(
             backgroundColor: Colors.grey,
-
-            onPressed: (){
-            },
+            onPressed: () {},
             child: Icon(Icons.add),
           ),
-        )
-    );
+        ));
   }
 }
